b2bb663f528c5f2fbd7f91c3b00ae03aaf9a3636
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-11-03 17:06:08 +0100

    test: assert executor run

--- e2e/my-plugin-e2e/tests/my-plugin.spec.ts
+++ e2e/my-plugin-e2e/tests/my-plugin.spec.ts
@@ -5,17 +5,18 @@ import {
 } from '@nrwl/nx-plugin/testing';
 
 describe('my-plugin e2e', () => {
-  const projectName = uniq('test');
+  let projectName: string;
 
   beforeAll(async () => {
+    projectName = uniq('react-test-app');
     ensureNxProject('@project-config-bug/my-plugin', 'dist/packages/my-plugin');
     await runNxCommandAsync(
       `generate @nrwl/react:app ${projectName} --routing=false`
     );
   }, 600000);
 
-  afterAll(() => {
-    runNxCommandAsync('reset');
+  afterAll(async () => {
+    await runNxCommandAsync('reset');
   });
 
   it('should keep existing targets', async () => {
@@ -23,5 +24,7 @@ describe('my-plugin e2e', () => {
       `generate @project-config-bug/my-plugin:my-plugin ${projectName}`
     );
 
+    const foobarResult = await runNxCommandAsync(`run ${projectName}:foobar`);
+    expect(foobarResult.stdout).toContain('Executor ran for Foobar');
   }, 120000);
 });
